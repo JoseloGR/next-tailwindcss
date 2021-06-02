bad48a350a768b02606eab9264c7e9219c42841b
Jose Luis GR <A01063645@itesm.mx>
2021-06-02 13:18:08 -0500

    setup project with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      blueGray: colors.blueGray,
+      white: colors.white,
+      green: colors.green,
+      gray: colors.gray,
+      red: colors.red,
+      blue: colors.blue
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      opacity: ['disabled']
+    },
   },
   plugins: [],
 }
